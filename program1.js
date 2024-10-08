a1eb4502618b79e02663a6452eee07c6c3b581f0
tanu241 <mehartanvi958@gmail.com>
2024-10-08 14:14:21 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -34,7 +34,7 @@ const testString=[
     "}{}",
 ]; 
 testString.forEach((str, index)=>{
-    console.log('Input: s= "${string')
+    console.log('Input: s= "${str')
 })
 
 module.exports = { isValid };
